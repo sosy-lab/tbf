2b71cefe8e22e65fd29ac1ff23882b5169be1230
Leo Strakosch <lembergerth@gmail.com>
2017-05-25 20:26:52 +0200

    Header files for corresponding __VERIFIER_nondet_X() calls

---
+++ include/svcomp_klee_extended.h
@@ -0,0 +1,25 @@
+loff_t __VERIFIER_nondet_loff_t() {
+    loff_t __symbolic_loff_t;
+    klee_make_symbolic(&__symbolic_loff_t, sizeof(__symbolic_loff_t), "__symbolic_loff_t");
+    return __symbolic_loff_t;
+}
+u32 __VERIFIER_nondet_u32() {
+    u32 __symbolic_u32;
+    klee_make_symbolic(&__symbolic_u32, sizeof(__symbolic_u32), "__symbolic_u32");
+    return __symbolic_u32;
+}
+pchar __VERIFIER_nondet_pchar() {
+    pchar __symbolic_pchar;
+    klee_make_symbolic(&__symbolic_pchar, sizeof(__symbolic_pchar), "__symbolic_pchar");
+    return __symbolic_pchar;
+}
+size_t __VERIFIER_nondet_size_t() {
+    size_t __symbolic_size_t;
+    klee_make_symbolic(&__symbolic_size_t, sizeof(__symbolic_size_t), "__symbolic_size_t");
+    return __symbolic_size_t;
+}
+sector_t __VERIFIER_nondet_sector_t() {
+    sector_t __symbolic_sector_t;
+    klee_make_symbolic(&__symbolic_sector_t, sizeof(__symbolic_sector_t), "__symbolic_sector_t");
+    return __symbolic_sector_t;
+}
