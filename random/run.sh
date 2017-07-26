ef8858552c59454dfad4ead6ee57a50739d8b713
Leo Strakosch <lembergerth@gmail.com>
2017-07-27 00:00:31 +0200

    Fix and add error handling in random tester

--- random/run.sh
+++ random/run.sh
@@ -3,7 +3,12 @@
 PROG=$1
 COUNT=0
 if [[ -z $PROG ]]; then
-  &>2 echo "Usage: run.sh PROG"
+  >&2 echo "Usage: run.sh PROG"
+  exit
+fi
+if [[ ! -x $PROG ]]; then
+  >&2 echo "Given file not executable"
+  exit
 fi
 
 while true; do
