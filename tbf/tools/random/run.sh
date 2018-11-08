502a5cac35f30e7628995842948d1c95e0c5e1a8
Thomas Lemberger <lembergerth@gmail.com>
2018-11-08 16:52:57 +0100

    Make random tester more robust for given executables
    Doesn't require directory of executable or member
    of PATH anymore, but executable may be in current
    directory

--- tbf/tools/random/run.sh
+++ tbf/tools/random/run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-PROG=$1
+PROG="$1"
 COUNT=0
-if [[ -z $PROG ]]; then
+if [[ -z "$PROG" ]]; then
   >&2 echo "Usage: run.sh PROG"
   exit
 fi
-if [[ ! -x $PROG ]]; then
+if [[ ! -x "$PROG" ]]; then
   >&2 echo "Given file not executable"
   exit
 fi
 
+if ! command -v "$PROG"; then
+    PROG="./${PROG}"
+fi
+
 while true; do
   touch 'vector.test'
-  $PROG
+  "$PROG"
   mv 'vector.test' "vector${COUNT}.test"
   (( COUNT += 1 ))
 done
