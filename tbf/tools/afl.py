sosy-lab/tbf

commit a1948a4d8e116117459d3d239fcd9e48b4d9b004

Preprocessing in own class, remove dependencies
Only dependency of tool-specific modules
are base classes and utils package.

This is a first step to put these classes in their
own, independent module.

tbf/tools/afl.py
@@ -3,7 +3,6 @@ import logging
 import os
 
 import tbf.utils as utils
-from tbf.harness_generation import HarnessCreator
 from tbf.input_generation import BaseInputGenerator
 from tbf.testcase_converter import TestConverter
 
@@ -12,6 +11,75 @@ bin_dir = os.path.join(module_dir, 'afl/bin')
 findings_dir = './findings'
 name = 'afl-fuzz'
 tests_dir = '.'
+
+
+class Preprocessor:
+
+    def prepare(self, filecontent, nondet_methods_used, error_method=None):
+        content = filecontent
+        content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        content += '\n'
+        content += self._get_vector_read_method()
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
+            content += nondet_method_definition
+        return content
+
+    @staticmethod
+    def _get_vector_read_method():
+        return """char * parse_inp(char * __inp_var) {
+        unsigned int input_length = strlen(__inp_var)-1;
+        /* Remove '\\n' at end of input */
+        if (__inp_var[input_length] == '\\n') {
+            __inp_var[input_length] = '\\0';
+        }
+
+        char * parseEnd;
+        char * value_pointer = malloc(16);
+
+        unsigned long long intVal = strtoull(__inp_var, &parseEnd, 0);
+        if (*parseEnd != 0) {
+          long long sintVal = strtoll(__inp_var, &parseEnd, 0);
+          if (*parseEnd != 0) {
+            long double floatVal = strtold(__inp_var, &parseEnd);
+            if (*parseEnd != 0) {
+              fprintf(stderr, "Can't parse input: '%s' (failing at '%s')\\n", __inp_var, parseEnd);
+              abort();
+
+            } else {
+              memcpy(value_pointer, &floatVal, 16);
+            }
+          } else {
+            memcpy(value_pointer, &sintVal, 8);
+          }
+        } else {
+          memcpy(value_pointer, &intVal, 8);
+        }
+
+        return value_pointer;
+    }\n\n"""
+
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, method_param):
+        definition = ""
+        definition += utils.get_method_head(method_name, method_type,
+                                            method_param)
+        definition += ' {\n'
+        if method_type != 'void':
+            definition += "    unsigned int inp_size = 3000;\n"
+            definition += "    char * inp_var = malloc(inp_size);\n"
+            definition += "    fgets(inp_var, inp_size, stdin);\n"
+
+            definition += "    return *((" + method_type + "*) parse_inp(inp_var));\n"
+        definition += ' }\n'
+        return definition
 
 
 class InputGenerator(BaseInputGenerator):
@@ -61,14 +129,6 @@ class InputGenerator(BaseInputGenerator):
             env['AFL_SKIP_CPUFREQ'] = 'true'
         return env
 
-    def prepare(self, filecontent, nondet_methods_used):
-        harness_creator = HarnessCreator()
-        harness = harness_creator._get_vector_read_method()
-        harness += harness_creator._get_nondet_method_definitions(
-            nondet_methods_used, None)
-        content = filecontent + '\n' + harness.decode()
-        return content
-
     def _get_compiler(self):
         env = self.get_run_env()
         if 'AFL_CC' in env.keys():
@@ -108,7 +168,7 @@ class AflTestConverter(TestConverter):
             content = inp.read()
         utils.TestCase(test_name, test_file, content)
 
-    def get_test_vector(self, test_case, nondet_methods):
+    def get_test_vector(self, test_case):
         vector = utils.TestVector(test_case.name, test_case.origin)
         for line in test_case.content.split(b'\n'):
             vector.add(line)

tbf/input_generation.py
@@ -25,10 +25,12 @@ class BaseInputGenerator(object):
     def failed(result):
         return result.returncode != 0
 
-    def __init__(self, machine_model, log_verbose, additional_options):
+    def __init__(self, machine_model, log_verbose, additional_options, preprocessor):
         self.machine_model = machine_model
         self.log_verbose = log_verbose
         self.cli_options = additional_options
+        self.program_preprocessor = preprocessor
+
         self.statistics = utils.Statistics("Input Generator " + self.get_name())
 
         self.timer_file_access = utils.Stopwatch()
@@ -44,24 +46,6 @@ class BaseInputGenerator(object):
                                   self.timer_file_access)
         self.statistics.add_value('Time for file preparation',
                                   self.timer_prepare)
-
-    @abstractmethod
-    def prepare(self, filecontent, nondet_methods_used):
-        pass
-
-    def prepare0(self, filecontent, error_method, nondet_methods):
-        content = filecontent
-        content = utils.rewrite_cproblems(content)
-        content += '\n'
-        content += utils.EXTERNAL_DECLARATIONS
-        if error_method:
-            content += self._get_error_method_dummy(error_method)
-        content += utils.get_assume_method()
-        return self.prepare(content, nondet_methods)
-
-    def _get_error_method_dummy(self, error_method):
-        return 'void ' + error_method + '() {{ fprintf(stderr, \"{0}\\n\"); exit(1); }}\n'.format(
-            utils.error_string)
 
     def generate_input(self, filename, error_method, nondet_methods, stop_flag):
         default_err = "Unknown error"
@@ -79,7 +63,7 @@ class BaseInputGenerator(object):
                     "Prepared file already exists. Not preparing again.")
             else:
                 self.timer_prepare.start()
-                prepared_content = self.prepare0(filecontent, error_method, nondet_methods)
+                prepared_content = self.program_preprocessor.prepare(filecontent, nondet_methods, error_method)
                 self.timer_file_access.start()
                 with open(file_to_analyze, 'w+') as new_file:
                     new_file.write(prepared_content)
@@ -109,7 +93,7 @@ class BaseInputGenerator(object):
         except utils.InputGenerationError as e:
             # Should be error because an error in input generation is not expected
             logging.error("Input generation error: %s", e.msg
-                          if e.msg else default_err)
+            if e.msg else default_err)
             return self._get_failed_and_stats()
         except utils.ParseError as e:
             logging.error("Parse error: %s", e.msg if e.msg else default_err)

tbf/tools/cpatiger.py
@@ -13,41 +13,29 @@ input_method = 'input'
 name = 'cpatiger'
 
 
-class InputGenerator(BaseInputGenerator):
+class Preprocessor:
 
-    def __init__(self,
-                 timelimit=None,
-                 log_verbose=False,
-                 additional_cli_options='',
-                 machine_model=utils.MACHINE_MODEL_32):
-        super().__init__(machine_model, log_verbose, additional_cli_options)
-
-        self._run_env = utils.get_env_with_path_added(binary_dir)
-        # Make sure that timelimit is never None
-        self.timelimit = timelimit if timelimit else 0
-
-    def get_run_env(self):
-        return self._run_env
-
-    def get_name(self):
-        return name
-
-    def prepare(self, filecontent, nondet_methods_used):
+    def prepare(self, filecontent, nondet_methods_used, error_method=None):
         content = filecontent
         content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        content += '\n'
         content += 'extern int input();\n'
-        for method in nondet_methods_used:  # append method definition at end of file content
-            nondet_method_definition = self._get_nondet_method(method)
+        content += ''
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
             content += nondet_method_definition
+
         return content
 
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, param_types):
         method_head = utils.get_method_head(method_name, method_type, param_types)
         method_body = ['{']
         if method_type != 'void':
@@ -58,6 +46,26 @@ class InputGenerator(BaseInputGenerator):
         method_body += '\n}\n'
 
         return method_head + method_body
+
+
+class InputGenerator(BaseInputGenerator):
+
+    def __init__(self,
+                 timelimit=None,
+                 log_verbose=False,
+                 additional_cli_options='',
+                 machine_model=utils.MACHINE_MODEL_32):
+        super().__init__(machine_model, log_verbose, additional_cli_options, Preprocessor())
+
+        self._run_env = utils.get_env_with_path_added(binary_dir)
+        # Make sure that timelimit is never None
+        self.timelimit = timelimit if timelimit else 0
+
+    def get_run_env(self):
+        return self._run_env
+
+    def get_name(self):
+        return name
 
     def create_input_generation_cmds(self, filename, cli_options):
         import shutil

tbf/tools/crest.py
@@ -15,56 +15,27 @@ test_name_pattern = re.compile('input[0-9]+$')
 tests_dir = '.'
 
 
-class InputGenerator(BaseInputGenerator):
+class Preprocessor:
 
-    def __init__(self,
-                 log_verbose=False,
-                 additional_cli_options="",
-                 machine_model=utils.MACHINE_MODEL_32):
-        super().__init__(machine_model, log_verbose, additional_cli_options)
-        self.log_verbose = log_verbose
-
-        self._run_env = utils.get_env_with_path_added(bin_dir)
-        self._run_env = utils.add_ld_path_to_env(self._run_env, lib_dir)
-
-        self.num_iterations = 100000
-
-    def get_name(self):
-        return name
-
-    def get_run_env(self):
-        return self._run_env
-
-    def prepare(self, filecontent, nondet_methods_used):
-        content = ''
-        for line in filecontent.split('\n'):
-            prepared_line = line
-            if '//' in prepared_line:
-                start = prepared_line.find('//')
-                prepared_line = prepared_line[:start]
-            content += prepared_line + '\n'
-        content = '#include<crest.h>\n' + content
+    def prepare(self, filecontent,  nondet_methods_used, error_method=None):
+        content = filecontent
         content += '\n'
-        for method in nondet_methods_used:  # append method definition at end of file content
-            nondet_method_definition = self._get_nondet_method(method)
+        content += '#include<crest.h>\n'
+        content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        content += '\n'
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
             content += nondet_method_definition
         return content
 
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def is_supported_type(self, method_type):
-        return method_type in [
-            '_Bool', 'long long', 'long long int', 'long', 'long int', 'int',
-            'short', 'char', 'unsigned long long', 'unsigned long long int',
-            'unsigned long', 'unsigned long int', 'unsigned int',
-            'unsigned short', 'unsigned char'
-        ]
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
+    def _get_nondet_method_definition(self, method_name, method_type, param_types):
         if not (method_type == 'void' or self.is_supported_type(method_type)):
             logging.warning('Crest can\'t handle symbolic values of type %s',
                             method_type)
@@ -96,6 +67,36 @@ class InputGenerator(BaseInputGenerator):
         method_body += '\n}\n'
 
         return method_head + method_body
+
+    @staticmethod
+    def is_supported_type(method_type):
+        return method_type in [
+            '_Bool', 'long long', 'long long int', 'long', 'long int', 'int',
+            'short', 'char', 'unsigned long long', 'unsigned long long int',
+            'unsigned long', 'unsigned long int', 'unsigned int',
+            'unsigned short', 'unsigned char'
+        ]
+
+
+class InputGenerator(BaseInputGenerator):
+
+    def __init__(self,
+                 log_verbose=False,
+                 additional_cli_options="",
+                 machine_model=utils.MACHINE_MODEL_32):
+        super().__init__(machine_model, log_verbose, additional_cli_options, Preprocessor())
+        self.log_verbose = log_verbose
+
+        self._run_env = utils.get_env_with_path_added(bin_dir)
+        self._run_env = utils.add_ld_path_to_env(self._run_env, lib_dir)
+
+        self.num_iterations = 100000
+
+    def get_name(self):
+        return name
+
+    def get_run_env(self):
+        return self._run_env
 
     def create_input_generation_cmds(self, filename, cli_options):
         compile_cmd = [os.path.join(bin_dir, 'crestc'), filename]

tbf/tools/dummy.py
@@ -5,7 +5,41 @@ from tbf.testcase_converter import TestConverter
 name = "Dummy"
 
 
+class Preprocessor:
+
+    def prepare(self, filecontent,  nondet_methods_used, error_method=None):
+        content = filecontent
+        content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
+            content += nondet_method_definition
+        return content
+
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, param_types):
+        method_head = utils.get_method_head(method_name, method_type, param_types)
+        method_body = ['{']
+        if method_type != 'void':
+            method_body += [
+                'return *(({}*) 0);'.format(method_type)
+            ]
+        method_body = '\n    '.join(method_body)
+        method_body += '\n}\n'
+
+        return method_head + method_body
+
+
 class InputGenerator(BaseInputGenerator):
+
+    def __init__(self, machine_model, log_verbose, additional_options):
+        super().__init__(machine_model, log_verbose, additional_options, Preprocessor())
 
     def create_input_generation_cmds(self, program_file, cli_options):
         instrumented_program = './tested.out'
@@ -19,30 +53,6 @@ class InputGenerator(BaseInputGenerator):
 
     def get_name(self):
         return name
-
-    def prepare(self, filecontent, nondet_methods_used):
-        harness = ""
-        for nondet_method in nondet_methods_used:
-            harness = harness + self._get_nondet_method(nondet_method)
-        return filecontent + '\n' + harness
-
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
-        method_head = utils.get_method_head(method_name, method_type, param_types)
-        method_body = ['{']
-        if method_type != 'void':
-            method_body += [
-                'return *(({}*) 0);'.format(method_type)
-            ]
-        method_body = '\n    '.join(method_body)
-        method_body += '\n}\n'
-
-        return method_head + method_body
 
 
 class DummyTestConverter(TestConverter):

tbf/tools/fshell.py
@@ -14,32 +14,30 @@ tests_dir = '.'
 tests_file = os.path.join(tests_dir, 'testsuite.txt')
 
 
-class InputGenerator(BaseInputGenerator):
+class Preprocessor:
 
-    def get_run_env(self):
-        return utils.get_env_with_path_added(bin_dir)
-
-    def get_name(self):
-        return name
-
-    def prepare(self, filecontent, nondet_methods_used):
+    def prepare(self, filecontent, nondet_methods_used, error_method=None):
         content = filecontent
         content += '\n'
-        for m in nondet_methods_used:
-            content += self._get_nondet_method(m)
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        content += '\n'
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
+            content += nondet_method_definition
 
         # FShell ignores __VERIFIER_nondet methods. We rename them so that they are analyzed correctly
-        content = content.replace("__VERIFIER_", "___VERIFIER_")
+        content = content.replace("__VERIFIER_nondet", "___VERIFIER_nondet")
 
         return content
 
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, param_types):
         var_name = utils.get_sym_var_name(method_name)
         method_head = utils.get_method_head(method_name, method_type,
                                             param_types)
@@ -62,10 +60,21 @@ class InputGenerator(BaseInputGenerator):
 
         return method_head + method_body
 
-    def _get_error_method_dummy(self, error_method):
-        # overwrite the default error method dummy to *not* exit. Somehow, Fshell doesn't like exit or aborts.
-        return 'void ' + error_method + '() {{ fprintf(stderr, \"{0}\\n\"); }}\n'.format(
-            utils.error_string)
+    @staticmethod
+    def _get_error_method_definition(error_method):
+        return 'void ' + error_method + '() {{ fprintf(stderr, \"{0}\\n\"); }}\n'.format(utils.error_string)
+
+
+class InputGenerator(BaseInputGenerator):
+
+    def __init__(self, machine_model, log_verbose, additional_options):
+        super().__init__(machine_model, log_verbose, additional_options, Preprocessor())
+
+    def get_run_env(self):
+        return utils.get_env_with_path_added(bin_dir)
+
+    def get_name(self):
+        return name
 
     def create_input_generation_cmds(self, filename, cli_options):
         if self.machine_model.is_32:

tbf/tools/klee.py
@@ -15,6 +15,41 @@ klee_make_symbolic = 'klee_make_symbolic'
 name = 'klee'
 
 
+class Preprocessor:
+
+    def prepare(self, filecontent, nondet_methods_used, error_method=None):
+        content = filecontent
+        content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
+        for method in nondet_methods_used:
+            # append method definition at end of file content
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
+            content += nondet_method_definition
+        return content
+
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, param_types):
+        var_name = utils.get_sym_var_name(method_name)
+        method_head = utils.get_method_head(method_name, method_type,
+                                            param_types)
+        method_body = ['{']
+        if method_type != 'void':
+            method_body += [
+                method_type + ' ' + var_name + ';',
+                'klee_make_symbolic(&{0}, sizeof({0}), \"{0}\");'.format(
+                    var_name), 'return {0};'.format(var_name)
+            ]
+        method_body = '\n    '.join(method_body)
+        method_body += '\n}\n'
+
+        return method_head + method_body
+
+
 class InputGenerator(BaseInputGenerator):
 
     def __init__(self,
@@ -22,7 +57,7 @@ class InputGenerator(BaseInputGenerator):
                  log_verbose=False,
                  additional_cli_options="",
                  machine_model=utils.MACHINE_MODEL_32):
-        super().__init__(machine_model, log_verbose, additional_cli_options)
+        super().__init__(machine_model, log_verbose, additional_cli_options, Preprocessor())
         self.log_verbose = log_verbose
 
         run_env = utils.get_env_with_path_added(bin_dir)
@@ -36,37 +71,6 @@ class InputGenerator(BaseInputGenerator):
 
     def get_name(self):
         return name
-
-    def prepare(self, filecontent, nondet_methods_used):
-        content = filecontent
-        content += '\n'
-        for method in nondet_methods_used:
-            # append method definition at end of file content
-            nondet_method_definition = self._get_nondet_method(method)
-            content += nondet_method_definition
-        return content
-
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
-        var_name = utils.get_sym_var_name(method_name)
-        method_head = utils.get_method_head(method_name, method_type,
-                                            param_types)
-        method_body = ['{']
-        if method_type != 'void':
-            method_body += [
-                '{0} {1};'.format(method_type, var_name),
-                'klee_make_symbolic(&{0}, sizeof({0}), \"{0}\");'.format(
-                    var_name), 'return {0};'.format(var_name)
-            ]
-        method_body = '\n    '.join(method_body)
-        method_body += '\n}\n'
-
-        return method_head + method_body
 
     def create_input_generation_cmds(self, filename, cli_options):
         if self.machine_model.is_32:

tbf/tools/random_tester.py
@@ -13,30 +13,26 @@ generator_harness = module_dir / "random" / "random_tester.c"
 random_runner = module_dir / "random" / "run.sh"
 
 
-class InputGenerator(BaseInputGenerator):
+class Preprocessor:
 
-    def get_run_env(self):
-        return utils.get_env()
-
-    def get_name(self):
-        return name
-
-    def prepare(self, filecontent, nondet_methods_used):
+    def prepare(self, filecontent, nondet_methods_used, error_method=None):
         content = filecontent
         content += '\n'
+        content += utils.EXTERNAL_DECLARATIONS
+        content += '\n'
+        content += utils.get_assume_method()
+        content += '\n'
+        if error_method:
+            content += utils.get_error_method_definition(error_method)
         for method in nondet_methods_used:
             # append method definition at end of file content
-            nondet_method_definition = self._get_nondet_method(method)
+            nondet_method_definition = self._get_nondet_method_definition(method['name'], method['type'],
+                                                                          method['params'])
             content += nondet_method_definition
         return content
 
-    def _get_nondet_method(self, method_information):
-        method_name = method_information['name']
-        m_type = method_information['type']
-        param_types = method_information['params']
-        return self._create_nondet_method(method_name, m_type, param_types)
-
-    def _create_nondet_method(self, method_name, method_type, param_types):
+    @staticmethod
+    def _get_nondet_method_definition(method_name, method_type, param_types):
         var_name = utils.get_sym_var_name(method_name)
         method_head = utils.get_method_head(method_name, method_type,
                                             param_types)
@@ -51,6 +47,18 @@ class InputGenerator(BaseInputGenerator):
         method_body += '\n}\n'
 
         return method_head + method_body
+
+
+class InputGenerator(BaseInputGenerator):
+
+    def __init__(self, machine_model, log_verbose, additional_options):
+        super().__init__(machine_model, log_verbose, additional_options, Preprocessor())
+
+    def get_run_env(self):
+        return utils.get_env()
+
+    def get_name(self):
+        return name
 
     def create_input_generation_cmds(self, filename, cli_options):
         compiled_file = '.'.join(os.path.basename(filename).split('.')[:-1])

commit bf337ab6cf7d878cac71d36d8bd06d980a8b3eb3

Fix runtime error introduced in a1948a4d

tbf/tools/afl.py
@@ -84,6 +84,9 @@ class Preprocessor:
 
 class InputGenerator(BaseInputGenerator):
 
+    def __init__(self, machine_model, log_verbose, additional_options):
+        super().__init__(machine_model, log_verbose, additional_options, Preprocessor())
+
     def create_input_generation_cmds(self, program_file, cli_options):
         instrumented_program = './tested.out'
         compiler = self._get_compiler()